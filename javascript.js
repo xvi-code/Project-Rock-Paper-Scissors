ce1c80ebcb24cb7c1049b021a22111927a5feab8
Nicole <ziyizhuo@gmail.com>
2022-10-07 19:08:17 +0800

    .

--- javascript.js
+++ javascript.js
@@ -5,3 +5,9 @@ const choices = ['rock', 'paper', 'scissors'];
 function getComputerChoice () {
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+// Write a function that plays a single round of Rock Paper Scissors. 
+// The function should take two parameters - the playerSelection and computerSelection - 
+// and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+
